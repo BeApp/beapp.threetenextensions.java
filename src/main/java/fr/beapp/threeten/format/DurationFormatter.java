c238d58f6b893d09c36f493273512c6db48572ee
Damien Villeneuve <d.villeneuve@beapp.fr>
2018-02-27 10:11:44 +0100

    Make DurationFormatter immutable

--- src/main/java/fr/beapp/threeten/format/DurationFormatter.java
+++ src/main/java/fr/beapp/threeten/format/DurationFormatter.java
@@ -7,25 +7,24 @@ import java.util.Locale;
 
 public class DurationFormatter {
 
-	private final List<DurationFormatterBuilder.DurationPrinter> printers;
-	private Locale locale;
+    private final List<DurationFormatterBuilder.DurationPrinter> printers;
+    private final Locale locale;
 
-	public DurationFormatter(List<DurationFormatterBuilder.DurationPrinter> printers, Locale locale) {
-		this.printers = printers;
-		this.locale = locale;
-	}
+    public DurationFormatter(List<DurationFormatterBuilder.DurationPrinter> printers, Locale locale) {
+        this.printers = printers;
+        this.locale = locale;
+    }
 
-	public DurationFormatter withLocale(Locale locale) {
-		this.locale = locale;
-		return this;
-	}
+    public DurationFormatter withLocale(Locale locale) {
+        return new DurationFormatter(printers, locale);
+    }
 
-	public String format(Duration duration) {
-		StringBuilder builder = new StringBuilder();
-		for (DurationFormatterBuilder.DurationPrinter printer : printers) {
-			printer.print(printers, duration, locale, builder);
-		}
-		return builder.toString();
-	}
+    public String format(Duration duration) {
+        StringBuilder builder = new StringBuilder();
+        for (DurationFormatterBuilder.DurationPrinter printer : printers) {
+            printer.print(printers, duration, locale, builder);
+        }
+        return builder.toString();
+    }
 
 }
